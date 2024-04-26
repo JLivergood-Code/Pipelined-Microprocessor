f5f47a18577636ca70984b98a6e0d6774cd4bb39
JLivergood-Code <113878555+JLivergood-Code@users.noreply.github.com>
2024-04-26 00:36:39 -0700

    Update otter_mcu_pipeline_template_v2.sv
    Almost Completely Working

--- otter_mcu_pipeline_template_v2.sv
+++ otter_mcu_pipeline_template_v2.sv
@@ -59,16 +59,19 @@ module OTTER_MCU(input CLK,
 );           
     wire [6:0] opcode;
     wire [31:0] pc, pc_value, next_pc, jalr_pc, branch_pc, jump_pc, int_pc,A,B,
-        I_immed,S_immed,U_immed,aluBin,aluAin,aluResult,rfIn,csr_reg, mem_data;
+        I_immed,S_immed,U_immed, B_immed, J_immed,
+        aluBin,aluAin,aluResult,rfIn,csr_reg, mem_data;
     
     wire [31:0] IR;
     wire memRead1,memRead2;
     
     wire pcWrite,regWrite,memWrite, op1_sel,mem_op,IorD,pcWriteCond,memRead;
     wire [1:0] opB_sel, rf_sel, wb_sel, mSize;
-    logic [1:0] pc_sel;
+    logic [2:0] pc_sel, pc_source;
     wire [3:0]alu_fun;
     wire opA_sel;
+    
+    logic [31:0] wd;
     
     logic br_lt,br_eq,br_ltu;
      
@@ -86,6 +89,7 @@ module OTTER_MCU(input CLK,
      
      assign pcWrite = 1'b1; 	//Hardwired high, assuming now hazards
      assign memRead1 = 1'b1; 	//Fetch new instruction every cycle
+     assign pc_source = 3'b0;
      
      PC PC_count  (.CLK(CLK), .RST(RESET), .PC_WRITE(pcWrite), .PC_SOURCE(pc_source),
         .JALR(jalr_pc), .JAL(jal_pc), .BRANCH(branch_pc), .MTVEC(32'b0), .MEPC(32'b0),
@@ -103,56 +107,61 @@ module OTTER_MCU(input CLK,
 
      
 //==== Instruction Decode ===========================================
-    logic [31:0] de_opA, de_ex_opA;
-    logic [31:0] de_opB, de_ex_opB;
+    logic de_opA, de_ex_opA;
+    logic [1:0] de_opB, de_ex_opB;
     
     logic [31:0] de_ex_rs2, de_rs2; 
     logic [31:0] de_rs1, de_ex_rs1;
 
+    logic [31:0] ex_J_immed, ex_I_immed, ex_U_immed, ex_B_immed, ex_S_immed;
     
-    
+   
     
     opcode_t OPCODE;
-    assign OPCODE_t = opcode_t'(opcode);
+    assign opcode = IR[6:0];
+    assign OPCODE = opcode_t'(opcode);
     
     assign de_inst.mem_type = IR[14:12];
     
     assign de_inst.rs1_addr=IR[19:15];
     assign de_inst.rs2_addr=IR[24:20];
     assign de_inst.rd_addr=IR[11:7];
-    assign de_inst.opcode=OPCODE;
+    assign de_inst.opcode = OPCODE;
+   assign de_inst.pc = if_de_pc;
    
-    assign de_inst.rs1_used=    de_inst.rs1 != 0
+    assign de_inst.rs1_used=    de_rs1 != 0
                                 && de_inst.opcode != LUI
                                 && de_inst.opcode != AUIPC
                                 && de_inst.opcode != JAL;
 
-    assign de_inst.rs2_used=    de_inst.rs1 != 0
+    assign de_inst.rs2_used=    de_rs2 != 0
                                 && de_inst.opcode != LUI
                                 && de_inst.opcode != AUIPC
                                 && de_inst.opcode != JAL
                                 && de_inst.opcode != OP_IMM; 
     logic ir30;
     assign ir30 = IR[30];
-    logic [6:0] opcode;
-    assign opcode = IR[6:0];
+    
     logic [2:0] funct;
     assign funct = IR[14:12];                            
     
-    BCG OTTER_BCG(.RS1(rs1), .RS2(de_ex_rs2), .BR_EQ(br_eq), .BR_LT(br_lt), .BR_LTU(br_ltu));
+    BCG OTTER_BCG(.RS1(de_rs1), .RS2(de_rs2), .BR_EQ(br_eq), .BR_LT(br_lt), .BR_LTU(br_ltu));
     
     // gather rs values, where do I send them? // 
     REG_FILE OTTER_REG_FILE(.CLK(CLK), .EN(wb_inst.regWrite), .ADR1(de_inst.rs2_addr), .ADR2(de_inst.rs1_addr), 
         .WA(wb_inst.rd_addr), .WD(wd), .RS1(de_rs1), .RS2(de_rs2));
     
     //DOUBLE CHECK BR VALUES LATER, MIGHT HAVE TO BE DELAYED FOR PIPELINE
-    CU_DCDR OTTER_DCDR (.IR_30(ir30), .IR_OPCODE(opcode), .IR_FUNCT(funct), .BR_EQ(br_eq), .BR_LT(br_lt),
+    CU_DCDR OTTER_DCDR (.IR_30(ir30), .IR_OPCODE(de_inst.opcode), .IR_FUNCT(funct), .BR_EQ(br_eq), .BR_LT(br_lt),
      .BR_LTU(br_ltu), .ALU_FUN(de_inst.alu_fun), .ALU_SRCA(de_opA), .ALU_SRCB(de_opB), .PC_SOURCE(pc_sel),
      .REG_WRITE(de_inst.regWrite), .MEM_WE(de_inst.memWrite), .MEM_RDEN2(de_inst.memRead2),
       .RF_WR_SEL(de_inst.rf_wr_sel));
 	
 	//STILL NEED IMMEADIATE GENERATOR //
-	
+	ImmediateGenerator OTTER_IMGEN(.IR(IR[31:7]), .U_TYPE(U_immed), .I_TYPE(I_immed), .S_TYPE(S_immed),
+        .B_TYPE(B_immed), .J_TYPE(J_immed));
+        
+        
 	always_ff @(posedge CLK) begin
         de_ex_opA <= de_opA;
         de_ex_opB <= de_opB;
@@ -161,24 +170,34 @@ module OTTER_MCU(input CLK,
         de_ex_rs1 <= de_rs1;
         de_ex_rs2 <= de_rs2;
         
+        ex_J_immed <= J_immed;
+        ex_I_immed <= I_immed;
+        ex_U_immed <= U_immed;
+        ex_B_immed <= B_immed;
+        ex_S_immed <= S_immed;
+        
+        //pc_source <= pc_sel;
      end
 //==== Execute ======================================================
      
      
      logic [31:0] ex_mem_rs2;
-     logic ex_mem_aluRes = 0;
+     logic [31:0] ex_mem_aluRes;
      
      logic [31:0] opA_forwarded;
      logic [31:0] opB_forwarded;
      
      //NEEDS ALUA AND ALUB SOURCE MUXES
-     TwoMux OTTER_ALU_MUXA(.ALU_SRC_A(de_ex_opA), .RS1(de_ex_rs1), .U_TYPE(U_immed), .SRC_A(aluAin));
-     FourMux OTTER_ALU_MUXB(.SEL(de_ex_opB), .ZERO(de_ex_rs2), .ONE(I_immed), 
-                            .TWO(S_immed), .THREE(pc_out), .OUT(aluBin));
+     TwoMux OTTER_ALU_MUXA(.ALU_SRC_A(de_ex_opA), .RS1(de_ex_rs1), .U_TYPE(ex_U_immed), .SRC_A(aluAin));
+     FourMux OTTER_ALU_MUXB(.SEL(de_ex_opB), .ZERO(de_ex_rs2), .ONE(ex_I_immed), 
+                            .TWO(ex_S_immed), .THREE(de_ex_inst.pc), .OUT(aluBin));
     
+    //Branch Addres Generator
+     BAG OTTER_BAG(.RS1(de_ex_rs1), .I_TYPE(ex_I_immed), .J_TYPE(ex_J_immed), .B_TYPE(ex_B_immed), .FROM_PC(pc_out),
+         .JAL(jal_pc), .JALR(jalr_pc), .BRANCH(branch_pc));
      
      // Creates a RISC-V ALU
-     ALU OTTER_ALU (de_ex_inst.alu_fun, de_ex_opA, de_ex_opB, aluResult); // the ALU
+     ALU OTTER_ALU(.SRC_A(aluAin), .SRC_B(aluBin), .ALU_FUN(de_ex_inst.alu_fun), .RESULT(aluResult));
      
      //BAG
      
@@ -191,7 +210,7 @@ module OTTER_MCU(input CLK,
 
 
 //==== Memory ======================================================
-      
+    logic [31:0] wb_dout2;  
      
     assign IOBUS_ADDR = ex_mem_aluRes;
     assign IOBUS_OUT = ex_mem_rs2;
@@ -199,13 +218,13 @@ module OTTER_MCU(input CLK,
     
     //Memory File 
     // memRead1 is from IF block, hardwired high
-    Memory OTTER_MEMORY(.MEM_CLK(CLK), .MEM_RDEN1(memRead1), .MEM_RDEN2('b0), 
-        .MEM_WE2('b0), .MEM_ADDR1(addr1), .MEM_ADDR2(IOBUS_ADDR), .MEM_DIN2(IOBUS_OUT), .MEM_SIZE(size),
-         .MEM_SIGN(sign), .IO_IN(IOBUS_IN), .IO_WR(IOBUS_WR), .MEM_DOUT1(IR), .MEM_DOUT2(dout2));
+    Memory OTTER_MEMORY(.MEM_CLK(CLK), .MEM_RDEN1(memRead1), .MEM_RDEN2(ex_mem_inst.memRead2), 
+        .MEM_WE2(ex_mem_inst.memWrite), .MEM_ADDR1(addr1), .MEM_ADDR2(IOBUS_ADDR), .MEM_DIN2(IOBUS_OUT), .MEM_SIZE(ex_mem_inst.mem_type[1:0]),
+         .MEM_SIGN(ex_mem_inst.mem_type[2]), .IO_IN(IOBUS_IN), .IO_WR(IOBUS_WR), .MEM_DOUT1(IR), .MEM_DOUT2(dout2));
  
     always_ff @(posedge CLK) begin
         wb_inst <= ex_mem_inst;
-        
+        wb_dout2 <= dout2;
     end
  
  
@@ -214,7 +233,7 @@ module OTTER_MCU(input CLK,
     
     
     FourMux OTTER_REG_MUX(.SEL(wb_inst.rf_wr_sel), 
-            .ZERO(pc_out_inc), .ONE(32'b0), .TWO(dout2), .THREE(IOBUS_ADDR),
+            .ZERO(wb_inst.pc), .ONE(32'b0), .TWO(wb_dout2), .THREE(IOBUS_ADDR),
             .OUT(wd));
 
 
