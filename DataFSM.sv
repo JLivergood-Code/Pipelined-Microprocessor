b1f15a702c7bf58b9fa1b5a9ce2daa17638d3cc5
JLivergood-Code <113878555+JLivergood-Code@users.noreply.github.com>
2024-06-09 15:51:06 -0700

    Lab 5 Completed
    Couple more bugs need solving

--- DataFSM.sv
+++ DataFSM.sv
@@ -20,54 +20,79 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module DataFSM(input hit, input miss, input CLK, input RST, input read, input write, input dirty, output logic load, output logic writeback, output logic FSM_Write,
-               output logic FSM_Read, output logic stall);
+module MEM_FSM(
+    input hit, 
+    input miss, 
+    input CLK, 
+    input RST, 
+    input read, 
+    input write, 
+    input dirty, 
+    output logic load, 
+    output logic writeback, 
+    output logic FSM_Write,
+    output logic FSM_Read,
+    output logic set_dirty, 
+    output logic stall,
+    output logic [1:0] counter);
 
     typedef enum{
         START,
         LOAD,
         WRITE_BACK,
-        MEM_WRITE
+        MEM_WRITE,
+        MEM_READ,
+        PROCESS
     } state_type;
     
 state_type PS, NS;
-parameter counter = 0;
+logic [1:0] count_next;
+
 
 always_ff @(posedge CLK) begin
-    if(RST == 1)
+    if(RST == 1) begin
         PS <= START;
-    else
+        counter <= 0;
+    end
+    else begin
         PS <= NS;
+        counter <= count_next;
+        
+    end
 end
 
 always_comb begin
-    load = 1'b1;
+    load = 1'b0;
     stall = 0;
     FSM_Write = 1'b0;
     FSM_Read = 1'b0;
     writeback = 1'b0;
+    count_next = counter;
+//    set_dirty = 'b0;
     case (PS)
         START: begin
             load = 1'b0;
             if(read) begin
                 if(hit) begin
-                    NS = START;
+                    NS = PROCESS;
                 end
                 else if(miss && dirty) begin
                     NS = WRITE_BACK;
                     stall = 1'b1;
                 end
                 else if(miss && ~dirty)begin 
-                    NS = LOAD;
+                    NS = MEM_READ;
                     stall = 1'b1;
                 end
                 else NS = START;
             end
             else if(write) begin
                 if(hit) begin
-                    NS = START;
+                    set_dirty = 'b1;
+                    NS = PROCESS;
                 end
                 else if(miss) begin
+                    set_dirty = 'b0;
                     NS = MEM_WRITE;
                     stall = 1'b1;
                 end
@@ -77,14 +102,23 @@ always_comb begin
             end
       end
 
+      PROCESS: begin
+            if(write) begin
+                if(hit) begin
+                    set_dirty = 'b0;
+                end
+             end
+             NS = START;
+      end
+
       WRITE_BACK: begin
-        counter = counter + 1;
+        count_next = counter + 'b1;
         stall = 1'b1;
         writeback = 1'b1;
         FSM_Write = 1'b1;
-        if(counter == 4) begin
-          NS = LOAD;
-          counter = 0;
+        if(counter == 3) begin
+          NS = MEM_READ;
+          count_next = 0;
         end        
         else begin
           NS = WRITE_BACK;
@@ -92,14 +126,23 @@ always_comb begin
       end
 
       MEM_WRITE: begin
-        NS = LOAD;
+        NS = MEM_READ;
+        FSM_Write = 1'b1;
         stall = 1'b1;
+      end
+      
+      MEM_READ: begin
+        load = 1'b1;
+        FSM_Read = 1'b1;
+        stall = 1'b1;
+        NS = LOAD;
       end
 
       LOAD: begin
+        set_dirty = 'b0;
         load = 1'b1;
-        FSM_Write = 1'b1;
-        NS = START;
+        FSM_Read = 1'b0;
+        NS = PROCESS;
         stall = 1'b1;
       end
 
